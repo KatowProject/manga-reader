fbb0122477dd95911428b1c53a2d854873583cfa
KatowProject <naufalfaqih443@gmail.com>
2021-08-28 22:37:04 +0700

    frontend 99%, add login page

---
+++ web/favorite.php
@@ -0,0 +1,107 @@
+<?php 
+    ob_start();
+    session_start();
+    if (!isset($_SESSION['user_id'])) {
+        header('location: /login.php');
+    }
+?>
+
+
+<!doctype html>
+<html lang="id">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="/assets/image/kato.png" />
+    <!--CSS-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" 
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/default.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" 
+        integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" 
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Favorites · Manga Reader</title>
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">
+            <img src="https://cdn.discordapp.com/emojis/750342786825584811.png" width="30" height="30"
+                class="d-inline-block align-top" alt="">Manga Reader</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto nav-flex-icons">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-user"></i>
+                    </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink-333">
+                    <a class="dropdown-item" href="/logout.php">Logout</a>
+                    <a class="dropdown-item" href="/favorite">Favorite</a>
+                </div>
+                </li>
+            </ul>
+                
+                
+            <!-- <form class="form-inline my-lg-0">
+                <input type="text" class="form-control" placeholder="Title...." id="search-input">
+                <div class="input-group-prepend">
+                    <button class="btn btn-outline-light" type="button" id="button-search">Search</button>
+                </div>
+            </form> -->
+        </div>
+    </nav>
+
+    <div class="container" id="mangas"></div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <button type="button" class="btn btn-dark btn-floating btn-lg" id="btn-back-to-top">
+        <i class="fas fa-arrow-up"></i>
+    </button>
+    <!-- Footer -->
+    <footer class="page-footer font-small mt-auto">
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2021 All Rights Reserved.
+        <a href="https://github.com/KatowProject/"> KatowProject</a>
+    </div>
+    <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <!-- Optional JavaScript -->
+    <script src="/assets/js/script.js"></script>
+
+    <?php echo "<script>getFavorite()</script>"?>
+
+</body>
+
+</html>
---
+++ web/login.php
@@ -0,0 +1,107 @@
+<?php  define('BASEPATH', dirname(__FILE__));
+    ob_start();
+    session_start();
+
+    require('./curl.php');
+
+    if (isset($_SESSION['user_id'])) {
+        header('Location: /index.php');
+    }
+
+    if (isset($_POST['submit'])) {
+
+        $user = $_POST['user'];
+        $pass = $_POST['pass'];
+
+        if ($user == "" || $pass == "") {
+            echo "<script>alert('Username tidak boleh kosong')</script>";
+        } else {
+            $body = json_encode([
+              'username' => $user,
+              'password' => $pass
+            ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+            
+            $response = auth($body);
+            
+            if ($response['success'] == true) {
+                $_SESSION['user_id'] = $response['data']['user_id'];
+                $_SESSION['username'] = $response['data']['username'];
+
+                header('Location: /index.php');
+            } else {
+                $err_message = $response['message'];
+                echo "<script>alert('$err_message')</script>";
+            }
+        }
+    }
+
+    function redirect($url, $statusCode = 303){
+      header('Location: ' . $url, true, $statusCode);
+      die();
+   }
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Login Page">
+    <meta name="author" content="Katoww">
+    <meta name="generator" content="w">
+    <link rel="icon" type="image/png" href="/assets/image/kato.png" />
+    <title>Login · Manga Reader</title>
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="/assets/css/signin.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+    
+<main class="form-signin">
+  <form action="" method="post">
+    <img class="mb-4" src="/assets/image/kato.png" alt="" width="75" height="75">
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div class="form-floating">
+      <input type="username" class="form-control" id="floatingInput" placeholder="name@example.com" name="user">
+      <label for="floatingInput">Username</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="pass" required>
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button type="submit" name="submit" value="submit" class="w-100 btn btn-lg btn-dark">Sign in</button>
+    <p class="mt-5 mb-3 text-muted">&copy; <?php echo date('Y'); ?></p>
+  </form>
+</main>
+    
+  </body>
+</html>
