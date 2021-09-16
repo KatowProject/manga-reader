de32ac95aae4bd5ed9ed118950f339b1d1679d37
KatowProject <naufalfaqih443@gmail.com>
2021-09-17 06:42:02 +0700

    logout gix

--- web/logout.php
+++ web/logout.php
@@ -1,4 +1,8 @@
 <?php
 session_destroy();
+setcookie('session', '', time() - 3600);
+setcookie('username', '', time() - 3600);
+setcookie('user_id', '', time() - 3600);
+
 header('location: /login');
 ?>
